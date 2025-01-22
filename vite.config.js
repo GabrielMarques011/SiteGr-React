dfb089a2060815c79ed048faafab93aca4cd891a
GabrielMarques011 <gmgabriellima@gmail.com>
2025-01-22 16:38:47 -0300

    Subindo correção de pontos no vite.config e no package

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'dist',
+    outDir: 'dist', // Pasta de saída do build
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // Isso pode ajudar a evitar problemas com múltiplos chunks
+      },
+    },
   },
   plugins: [react()],
 })
